51815590385c1d1b364b14fbf8bf25ecbd21ce1d
Jave01 <mumtaz.jave@gmail.com>
2025-03-01 13:36:43 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from './Header/Nav'
+import Carousels from './Carousels/Carousels'
+import Cards1 from './Cards1/Cards1'
+import Cards2 from './Cards2/Cards2'
+import Footer from './Footer/Footer'
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <Carousels />
+     <Cards1 />
+     <Cards2 />
+     <Footer />
+
+    </div>
+  )
+}
+
+export default App
